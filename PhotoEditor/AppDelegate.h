ad35894138ed7c92e1dfd682e81ad38042b3b9ea
Mark <agranatmarkit@gmail.com>
2019-10-12 19:35:37 +0300

    Initial Commit

---
+++ PhotoEditor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoEditor
+//
+//  Created by Марк on 12/10/2019.
+//  Copyright © 2019 Agranat Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoEditor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoEditor
+//
+//  Created by Марк on 12/10/2019.
+//  Copyright © 2019 Agranat Mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
